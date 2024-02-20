65159b23155e549f0293376adbf555c425e9b8d8
Vladyslavua <vladgershman@gmail.com>
2024-02-20 18:47:56 +0100

    feat : add components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Header from "@/components/header";
+import {HeroParallaxDemo} from "@/components/hero";
+import Work from "@/components/work";
 
 export default function Home() {
   return (
     <main>
+        <Header/>
+
+        <HeroParallaxDemo />
+
+        <Work />
+
 
     </main>
   );
