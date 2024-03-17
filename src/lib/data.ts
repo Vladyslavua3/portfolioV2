c6864e7b24b1083c9df9db1cecd6a1516bdc315d
Vladyslavua <vladgershman@gmail.com>
2024-03-17 15:31:29 +0100

    feat : update data

---
+++ src/lib/data.ts
@@ -0,0 +1,151 @@
+import {
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    threejs,
+    subfag,
+    inctagram,
+    cryptoHub,
+    carRent,
+    cms,
+    nextjs,
+    moe
+} from '../../images/'
+
+export const data = [
+    {
+        title: "Car Rent",
+        link: "https://gomoonbeam.com",
+        thumbnail:carRent,
+    },
+    {
+        title: "Cursor",
+        link: "https://cursor.so",
+        thumbnail:cms,
+    },
+    {
+        title: "Rogue",
+        link: "https://userogue.com",
+        thumbnail:cryptoHub,
+    },
+
+    {
+        title: "Editorially",
+        link: "https://editorially.org",
+        thumbnail:inctagram,
+    },
+    {
+        title: "Editrix AI",
+        link: "https://editrix.ai",
+        thumbnail:moe,
+    },
+    {
+        title: "Pixel Perfect",
+        link: "https://app.pixelperfect.quest",
+        thumbnail:subfag,
+    },
+    {
+        title: "Pixel Perfect",
+        link: "https://app.pixelperfect.quest",
+        thumbnail:subfag,
+    },
+    {
+        title: "Pixel Perfect",
+        link: "https://app.pixelperfect.quest",
+        thumbnail:subfag,
+    },
+    {
+        title: "Pixel Perfect",
+        link: "https://app.pixelperfect.quest",
+        thumbnail:subfag,
+    },
+
+];
+
+
+export const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Next JS",
+        icon: nextjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "docker",
+        icon: docker,
+    },
+];
+
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+
+export const workSection = 'Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly descried with links to code repositories.It reflect my ability to solve complex problems, work with different technologies, and manage projects effectively'
