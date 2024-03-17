44a9a8f99780e8b6b059d64a78143e2fc1994460
Vladyslavua <vladgershman@gmail.com>
2024-03-17 15:37:29 +0100

    feat: add skills section

---
+++ src/components/skills.tsx
@@ -0,0 +1,22 @@
+import {InfiniteMovingCards} from "@/components/ui/infinite-moving-cards";
+import {technologies} from "@/lib/data";
+
+
+const Skills = () => {
+    return (
+        <div className={'sm:px-16 px-6 sm:py-16 py-10'}>
+            <h1 className={ "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]"}>Skills</h1>
+            <div className={'flex items-center justify-center'}>
+                <InfiniteMovingCards
+                    items={technologies}
+                    direction="right"
+                    speed="slow"
+                />
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Skills;
