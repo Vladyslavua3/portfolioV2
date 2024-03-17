c509afac6fe71a996796a38cab1631fdcb85cf13
Vladyslavua <vladgershman@gmail.com>
2024-03-17 15:32:31 +0100

    feat : add images for skills section

---
+++ images/index.ts
@@ -0,0 +1,45 @@
+import carRent from "../images/car-rent.png";
+import cms from "../images/cms.png";
+import cryptoHub from "../images/cryptoHub.png";
+import inctagram from "../images/inctagram.png";
+import moe from "../images/moe.png";
+import subfag from "../images/subfag.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import nextjs from './tech/nextjs.png'
+
+
+export {
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    threejs,
+    subfag,
+    inctagram,
+    cryptoHub,
+    carRent,
+    cms,
+    nextjs,
+    moe
+}
