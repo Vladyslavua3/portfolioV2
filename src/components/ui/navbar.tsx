b5187b00c6368044210b70662cbef4a1a1225fc1
Vladyslavua <vladgershman@gmail.com>
2024-03-17 15:36:41 +0100

    style : change color during burger menu

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -48,12 +48,12 @@ const Navbar = () => {
 
                 <div className={`${!toggle ? 'hidden' : 'flex'} p-6
           black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px]
-          z-10 rounded-xl`}>
+          z-10 rounded-xl bg-gray-600`}>
                     <ul className='list-none flex justify-end items-start flex-col gap-4'>
                         {navLinks.map((link) => (
                             <li key={link.id} className={`
                   ${active === link.title
-                                ? 'text-white'
+                                ? 'text-violet-300'
                                 : 'text-secondary'}
                   font-poppins font-medium
                   cursor-pointer text-[16px]`}
