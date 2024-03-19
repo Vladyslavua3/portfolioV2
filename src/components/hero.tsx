61165d89dfb09e592e644674df2f145229000060
Vladyslavua <vladgershman@gmail.com>
2024-03-19 18:59:12 +0100

    feat: implemented hero parallax

---
+++ src/components/hero.tsx
@@ -0,0 +1,7 @@
+import {HeroParallax} from "@/components/ui/hero-parallax";
+import {data} from "@/lib/data";
+
+
+export function HeroParallaxDemo() {
+    return <HeroParallax products={data}/>;
+}
