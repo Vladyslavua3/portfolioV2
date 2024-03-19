b3a7796d60b3148dd57546a64853af1fbb1d6a28
Vladyslavua <vladgershman@gmail.com>
2024-03-19 19:02:22 +0100

    feat: implement skills and contact components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 import Header from "@/components/header";
 import {HeroParallaxDemo} from "@/components/hero";
 import Work from "@/components/work";
+import Contact from "@/components/contact";
+import Skills from "@/components/skills";
+
+
 
 export default function Home() {
+
   return (
     <main>
         <Header/>
 
         <HeroParallaxDemo />
 
+        <Skills />
+
         <Work />
 
+        <Contact
+            serviceId={process.env.SERVICE_ID ?? ''}
+            templateId={process.env.TEMPLATE_ID ?? ''}
+            publicKeyId={process.env.PUBLIC_KEY_EMAILJS ?? ''}
+        />
 
     </main>
   );
